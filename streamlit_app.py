b1664f0973e342e4ac0f6d79401f7dc696f5c374
Muhamad Gunawan <igunnawan25@gmail.com>
2024-12-17 15:10:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,20 @@
 import streamlit as st
 import pandas as pd
+import io
 
 st.title('🎈 Clustering with Gym Dataset')
-st.info('Website for Machine Learning Modell')
+st.info('Website for Machine Learning Model')
 
-with st.expander('Data') :
-  data = pd.read_csv('https://raw.githubusercontent.com/igunnawan31/data/refs/heads/main/gym_members_exercise_tracking.csv')
-  st.write("### Dataset Preview:")
-  st.dataframe(data.head())  # Display data preview
+# Load Data
+with st.expander('Data'):
+    # Load the dataset
+    data = pd.read_csv('https://raw.githubusercontent.com/igunnawan31/data/refs/heads/main/gym_members_exercise_tracking.csv')
 
-  st.write("### Data Information:")
-  buffer = []
-  data.info(buf=buffer.append)  # Capture the output of data.info()
-  info_str = "\n".join(buffer)  # Join the captured output as a single string
-  st.text(info_str)
+    st.write("### Dataset Preview:")
+    st.dataframe(data.head())  # Display the first few rows of the dataset
 
-  
+    st.write("### Data Information:")
+    buffer = io.StringIO()  # Create a string buffer
+    data.info(buf=buffer)  # Write data.info() to the buffer
+    info_str = buffer.getvalue()  # Get the content of the buffer as a string
+    st.text(info_str)
