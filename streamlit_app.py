caa4c594b61c6ee3c67e4b68f94d6d86a2e55a3e
Muhamad Gunawan <igunnawan25@gmail.com>
2024-12-17 15:12:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,5 @@ with st.expander('Data'):
     st.dataframe(data.head())  # Display the first few rows of the dataset
 
     st.write("### Data Information:")
-    buffer = io.StringIO()  # Create a string buffer
-    data.info(buf=buffer)  # Write data.info() to the buffer
     info_str = buffer.getvalue()  # Get the content of the buffer as a string
     st.text(info_str)
