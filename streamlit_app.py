8c5875a887f35b7f7ecea1bbf9c13e9bd0308bda
Muhamad Gunawan <igunnawan25@gmail.com>
2024-12-17 15:04:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+st.title('🎈 Clustering with Gym Dataset')
+st.Info('Website for Machine Learning Modell')
+df = pd.read_csv('https://raw.githubusercontent.com/igunnawan31/data/refs/heads/main/gym_members_exercise_tracking.csv')
+df
